9c854af7c8e8b15c086c761652ef7ec7ff2c9d1a
Lucas Marinho <marinholucasit@gmail.com>
2020-06-29 19:14:56 -0300

    Implementada a funcionalidade de pesquisa

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -10,15 +10,19 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   String _search;
   int _offSet = 0;
-  
-  Future<Map> _getGif() async{   
-    final _APIKEY        = '';
-    final _URL_TOPGIFS   = 'https://api.giphy.com/v1/gifs/trending?api_key=$_APIKEY&limit=20&rating=G';
-    final _URL_SEARCHGIF = 'https://api.giphy.com/v1/gifs/search?api_key=$_APIKEY&q=$_search&limit=20&offset=$_offSet&rating=G&lang=pt';
+
+  Future<Map> _getGif() async {
+    final _APIKEY = '';
+    final _URL_TOPGIFS =
+        'https://api.giphy.com/v1/gifs/trending?api_key=$_APIKEY&limit=20&rating=G';
+    final _URL_SEARCHGIF =
+        'https://api.giphy.com/v1/gifs/search?api_key=$_APIKEY&q=$_search&limit=19&offset=$_offSet&rating=G&lang=pt';
 
     http.Response response;
     if (_search == null)
       response = await http.get(_URL_TOPGIFS);
+    else
+      response = await http.get(_URL_SEARCHGIF);
 
     return json.decode(response.body);
   }
@@ -28,7 +32,8 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueGrey[900],
-        title: Image.network('https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif'),
+        title: Image.network(
+            'https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif'),
         centerTitle: true,
       ),
       backgroundColor: Colors.blueGrey[900],
@@ -49,59 +54,91 @@ class _HomeState extends State<Home> {
                 fontSize: 18,
               ),
               textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                });
+              },
             ),
           ),
           Expanded(
             child: FutureBuilder(
-              future: _getGif(),
-              builder: (context, snapShot){
-                switch(snapShot.connectionState){
-                  case ConnectionState.waiting:
-                  case ConnectionState.none:
-                    return Container(
-                      width: 200.0,
-                      height: 200.0,
-                      alignment: Alignment.center,
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                          Colors.white
+                future: _getGif(),
+                builder: (context, snapShot) {
+                  switch (snapShot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
                         ),
-                        strokeWidth: 5.0,
-                      ),
-                    ); 
-                  default:
-                    if (snapShot.hasError)
-                      return Container();
-                    else 
-                      return _gifTable(context, snapShot);
-                }
-              }
-            ),
+                      );
+                    default:
+                      if (snapShot.hasError)
+                        return Container();
+                      else
+                        return _gifTable(context, snapShot);
+                  }
+                }),
           ),
         ],
-      ),     
+      ),
     );
   }
 
-  Widget _gifTable(BuildContext context, AsyncSnapshot snapshot){
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  Widget _gifTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
-      padding: EdgeInsets.all(10.0),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 10.0,
-        mainAxisSpacing: 10.0,
-      ), 
-      itemCount: snapshot.data["data"].length,
-      itemBuilder: (context, index){
-        return GestureDetector(
-          child: Image.network(
-            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-            height: 300.0,
-            fit: BoxFit.cover,
-          ),          
-        );
-      }
-    );
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemCount: _getCount(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if (_search == null || index < snapshot.data["data"].length) {
+            return GestureDetector(
+              child: Image.network(
+                snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                height: 300.0,
+                fit: BoxFit.cover,
+              ),
+            );
+          } else {
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 70.0,
+                    ),
+                    Text(
+                      "More gifs ...",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+        });
   }
-
 }
